fc12389340b364e9842a171e54f5c058a4d95f6b
Dmitry Novikov <glowinglens@gmail.com>
2012-09-21 00:56:48 +0400

    add ie.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Template1::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(backend.js backend.css ie.css print.css)
+  config.assets.precompile += %w(backend.js backend.css ie.js ie.css print.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
