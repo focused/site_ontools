71774435b9715e70f2d619e345451ea131465b22
Dmitry Novikov <glowinglens@gmail.com>
2012-09-21 00:49:13 +0400

    try

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,12 +8,12 @@ class PagesController < ApplicationController
 
     @page = Page.by_path(params[:path])
     if @page.present?
-      meta = PageMetaTag.by_path(params[:path].to_s)
-      set_meta_tags(
-        title: (meta.try(:title).present? and !meta.title.nil?) ? meta.title.to_str : t.meta.title,
-        description: (meta.try(:description).present? and !meta.description.nil?) ? meta.description.to_str : t.meta.description,
-        keywords: (meta.try(:keywords).present? and !meta.keywords.nil?) ? meta.keywords.to_str : t.meta.keywords
-      )
+      # meta = PageMetaTag.by_path(params[:path].to_s)
+      # set_meta_tags(
+      #   title: (meta.try(:title).present? and !meta.title.nil?) ? meta.title.to_str : t.meta.title,
+      #   description: (meta.try(:description).present? and !meta.description.nil?) ? meta.description.to_str : t.meta.description,
+      #   keywords: (meta.try(:keywords).present? and !meta.keywords.nil?) ? meta.keywords.to_str : t.meta.keywords
+      # )
     elsif params[:path] == "simulate_exception"
       render file: "/simulate_exception"
     else
