d1a91f3dcc6535bfe850db9c84387ba18be7c863
Dmitry Novikov <glowinglens@gmail.com>
2014-06-25 17:41:48 +0400

    fix mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,15 +54,16 @@ SiteOne::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'ontools.ru' }
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              "smtp.gmail.com",
-  #   port:                 587,
-  #   domain:               'mdterra.org',
-  #   user_name:            'm.magomedoff@mdterra.org',
-  #   password:             '',
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "mail.locum.ru",
+    port:                 25,
+    domain:               'ontools.ru',
+    user_name:            'pletenets@ontools.ru',
+    password:             'hotfatdog88',
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # http://zsecft.mailinator.com/
   # M8R-dx70yu@mailinator.com
